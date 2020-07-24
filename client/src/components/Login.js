143f6a59261c39b92ad5d23c79c263d1485f0e01
BuildTools <unconfigured@null.spigotmc.org>
2020-07-24 08:42:44 -0700

    created axios with auth and private route

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+const defaultCredentials = {
+  username: '',
+  password: '',
+  
+}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [ credentials, setCredentials ] = useState()
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
